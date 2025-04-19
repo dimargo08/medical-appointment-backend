798949b80b53ad05f8b3ef2470ab8c0e7e2f6698
diego marcelo <dhmg.081093@gmail.com>
2025-04-19 11:00:09 -0500

    add env

--- tests/validator.test..ts
+++ tests/validator.test..ts
@@ -1,10 +1,48 @@
-import { validateAppointmentRequest } from '../utils/validators';
+import { validateAppointmentRequest } from '../src/utils/validators';
 
-test('Valida un request correcto', () => {
-  const result = validateAppointmentRequest({
-    insuredId: "01234",
-    scheduleId: 101,
-    countryISO: "PE"
+describe('validateAppointmentRequest', () => {
+  it('debe aceptar una solicitud válida', () => {
+    const result = validateAppointmentRequest({
+      insuredId: "01234",
+      scheduleId: 101,
+      countryISO: "PE"
+    });
+    expect(result.valid).toBe(true);
   });
-  expect(result.valid).toBe(true);
+
+  it('debe rechazar insuredId inválido', () => {
+    const result = validateAppointmentRequest({
+      insuredId: "1234", // solo 4 dígitos
+      scheduleId: 101,
+      countryISO: "PE"
+    });
+    expect(result.valid).toBe(false);
+    expect(result.message).toMatch(/insuredId/i);
+  });
+
+  it('debe rechazar scheduleId inválido', () => {
+    const result = validateAppointmentRequest({
+      insuredId: "01234",
+      scheduleId: -10,
+      countryISO: "PE"
+    });
+    expect(result.valid).toBe(false);
+    expect(result.message).toMatch(/scheduleId/i);
+  });
+
+  it('debe rechazar countryISO inválido', () => {
+    const result = validateAppointmentRequest({
+      insuredId: "01234",
+      scheduleId: 101,
+      countryISO: "AR" // no permitido
+    });
+    expect(result.valid).toBe(false);
+    expect(result.message).toMatch(/countryISO/i);
+  });
+
+  it('debe rechazar cuando el body es undefined', () => {
+    const result = validateAppointmentRequest(undefined);
+    expect(result.valid).toBe(false);
+    expect(result.message).toMatch(/Body vacío/);
+  });
 });
