4ccf45696021d226d25d017effb166ad90b78dc5
dimargo08 <dimargo.081093@gmail.com>
2025-04-19 08:55:58 -0500

    Add files via upload

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest'
+    },
+    collectCoverage: true,
+    coverageDirectory: 'coverage'
+  };
+  
